a045092070ebbe9a122404120ca77b52eb675f71
kevincanani <kevin.232122@alunosatc.edu.br>
2025-08-04 16:04:14 -0300

    Teste

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Test from './Screens/Test';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>AAAAAAAAAA</Text>
-      <StatusBar style="auto" />
+      <Test/>
     </View>
   );
 }
---
+++ Screens/Test.js
@@ -0,0 +1,16 @@
+import { View, Image, StyleSheet } from "react-native";
+
+export default function Test() {
+    return(
+        <View>
+            <Image style={styles.img} source={require('../assets/ornitorrinco.png')}/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    img:{
+        width: 100,
+        height: 200
+    }
+})
